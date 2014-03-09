d7258286f1f6dbf311a33942ce70f38cbc7904e1
rasikain <rasikain@yahoo.com>
2014-03-09 15:12:11 +0530

    Scorecard page added

---
+++ creative-crew-web-theme/javascript/main.js
@@ -0,0 +1,37 @@
+$("#launchButton").bind({
+    click: function () {
+        var slider_width = $('.cc-side-bar').width(); //get width automaticly
+        if ($(this).css("margin-left") == slider_width + "px" && !$(this).is(':animated')) {
+            $('.cc-side-bar,#launchButton, #pageLoader').animate({
+                "margin-left": '-=' + slider_width
+            });
+        } else {
+            if (!$(this).is(':animated')) //perevent double click to double margin
+            {
+                $('.cc-side-bar,#launchButton, #pageLoader').animate({
+                    "margin-left": '+=' + slider_width
+                });
+            }
+        }
+    }
+});
+
+$("#loadDashboard").bind({
+    click: function () {
+        $("#midContainer").load('index.html #appContent')
+    }
+});
+$("#loadScorecard").bind({
+    click: function () {
+
+        $("#midContainer").load('scorecard.html #appContent');
+    }
+});
+$("#loadTypography").bind({
+    click: function () {
+
+        $("#midContainer").load('typhography.html');
+    }
+});
+$("#headerContainer").load('header.html');
+$("#midContainer").load('index.html #appContent');
